e8a7bc72a2e11350f405cf6e4efde4abc373719c
Vivek Kumar <Vivekkumarmndl18@gmail.com>
2024-07-01 18:39:33 +0530

    made some changes

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: "https://e-campus.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
